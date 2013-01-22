3451b016b188a66b7d335f998c1b96f7ef6063f0
pratz <pratz0909@gmail.com>
2013-01-22 15:16:49 +0530

    added production settings file and edit ignore rules

---
+++ openring_site/production.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+
+"""
+Django settings variables for production purpose.
+"""
+
+# Debug settings
+DEBUG = True
+
+# Database settings
+DB_ENGINE = 'mysql'
+DB_NAME = ''
+DB_USER = ''
+DB_PASS = ''
+DB_HOST = ''
+DB_PORT = ''
--- openring_site/settings.py
+++ openring_site/settings.py
@@ -4,14 +4,14 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('pratz', 'pratz0909@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -77,7 +77,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -87,7 +87,7 @@ SECRET_KEY = 'n*k1z=9+b5douze=bkakk)o8$-4b$#34af@@ntdz8-qg_fld5j'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    #     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
