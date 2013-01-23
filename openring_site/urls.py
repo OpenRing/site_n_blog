415583aa3d562d743e169ba9f92fae54551e59bd
pratz <pratz0909@gmail.com>
2013-01-23 22:23:50 +0530

    added cms urls in base urls.py

--- openring_site/urls.py
+++ openring_site/urls.py
@@ -1,17 +1,24 @@
-from django.conf.urls import patterns, include, url
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+# python/django imports
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+# Required for django admin panel
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'openring_site.views.home', name='home'),
-    # url(r'^openring_site/', include('openring_site.foo.urls')),
+                       # Examples:
+                       # url(r'^$', 'openring_site.views.home', name='home'),
+                       # url(r'^openring_site/', include('openring_site.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Django docs
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+                       # Django admin
+                       url(r'^admin/', include(admin.site.urls)),
+
+                       # Django cms
+                       url(r'^', include('cms.urls')),
+                       )
