3dc174a115f32a3d1bab2767f41ed643f74a2d77
pratz <pratz0909@gmail.com>
2013-02-19 12:42:36 +0530

    create template tag to capture template tags as variables

---
+++ common_tags/templatetags/capture_as.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: ISO-8859-1 -*-
+"""
+Custome template tag to capture template tags as variables
+"""
+
+# python/django imports
+from django import template
+
+# project import
+
+
+register = template.Library()
+
+
+@register.tag(name='captureas')
+def do_captureas(parser, token):
+    try:
+        tag_name, args = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError("'captureas' node requires a variable name.")
+    nodelist = parser.parse(('endcaptureas',))
+    parser.delete_first_token()
+    return CaptureasNode(nodelist, args)
+
+
+class CaptureasNode(template.Node):
+    def __init__(self, nodelist, varname):
+        self.nodelist = nodelist
+        self.varname = varname
+
+    def render(self, context):
+        output = self.nodelist.render(context)
+        context[self.varname] = output
+        return ''
